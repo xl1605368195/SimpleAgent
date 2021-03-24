b815190dc14b00efecaa9b6b140c0f3e70cf5020
user01 <1870011370@qq.com>
2021-03-24 16:23:57 +0800

    Jar版本修改

--- src/main/java/com/demo/Agent.java
+++ src/main/java/com/demo/Agent.java
@@ -2,21 +2,17 @@ package com.demo;
 
 import java.lang.instrument.Instrumentation;
 
-/**
- * @author xule05
- * @date 2020/5/26 下午8:20
- */
 public class Agent {
-    public static void premain(String param, Instrumentation inst) {
-        main(param, inst);
+    public static void premain(String args, Instrumentation inst) {
+        main(args, inst);
     }
 
-    public static void agentmain(String param, Instrumentation inst) {
-        main(param, inst);
+    public static void agentmain(String args, Instrumentation inst) {
+        main(args, inst);
     }
 
     private synchronized static void main(String args, final Instrumentation inst) {
-        // 打印 attach时传入的参数
-        System.out.println("args: " + args);
+        // 打印attach时传入的参数
+        System.out.println("args=" + args);
     }
 }
